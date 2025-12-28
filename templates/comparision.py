9ca32a441abfc09cd9b0b3916c1fee84674e53b3
tonypowl <tonypaul0412@gmail.com>
2025-12-28 18:12:15 +0530

    added price difference output in comparision.py

--- templates/comparision.py
+++ templates/comparision.py
@@ -1,6 +1,9 @@
 #sample product in dataset vs random product X 
+from logic_blocks.compare_prices_block import compare_prices
+
 def comparison_template(product_a: dict, product_b: dict) -> dict:
     return {
         "product_a": product_a,
-        "product_b": product_b
+        "product_b": product_b,
+        "price_difference": compare_prices(product_a["price"], product_b["price"])
     }
